f47f4500165a24bbfd3711dd37177c1ba6e1d50d
ZJY <work@zjyu.de>
2023-03-29 14:09:57 +0800

    uploader 模型改动，保存解析后的结果

--- uploader/models.py
+++ uploader/models.py
@@ -6,14 +6,14 @@ User = get_user_model()
 
 class UploadFile(models.Model):
     origin_file_name = models.CharField(
-        max_length=300, verbose_name='原始文件名', null=True, blank=True
+        max_length=255, verbose_name='原始文件名', null=True, blank=True
     )
     file_name = models.CharField(
-        max_length=300, verbose_name='文件名', null=True, blank=True,
+        max_length=255, verbose_name='文件名', null=True, blank=True,
         help_text='文件在 GCS 中的名称', unique=True
     )
     gcs_path = models.CharField(
-        max_length=300, verbose_name='GCS路径', null=True, blank=True,
+        max_length=255, verbose_name='GCS路径', null=True, blank=True,
         help_text='文件在 GCS 中的路径', unique=True
     )
     upload_time = models.DateTimeField(
@@ -21,6 +21,10 @@ class UploadFile(models.Model):
     )
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, verbose_name='上传者', null=True, blank=True
+    )
+    context = models.TextField(
+        verbose_name='解析识别后的文件内容', null=True, blank=True,
+        help_text='解析识别后的文件内容'
     )
 
     class Meta:
